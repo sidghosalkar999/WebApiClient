d52003cca831c9e0aaacb8ecbffe678392a332d1
siddhesh <Admin@DESKTOP-V1RC23K>
2021-10-21 20:26:35 +0530

    Add project files.

---
+++ WebApiClient/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a % b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ WebApiClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            CalculatorService calc = new CalculatorService();
+            Console.WriteLine($"Addition :{calc.Add(4, 4)}");
+            Console.WriteLine($"Substraction :{calc.Substract(4, 4)}");
+            Console.WriteLine($"Multiplication :{calc.Multiply(4, 4)}");
+            Console.WriteLine($"Division :{calc.Division(4, 4)}");
+
+            Console.ReadLine();
+        }
+    }
+}
